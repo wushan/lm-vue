4739aaae755989362f9b4340fb7ed603c08d7b3a
Tony Chiang <chiangloveslynn@gmail.com>
2016-09-18 15:35:26 +0800

    update

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,15 +2,33 @@
 	main
 		#main
 			page-navigation
-			section#news
+			section#news-slider
 				.container.restrict
 					h2 News List
-					
+			section#news-list
+				.cover
+				.container.restrict-large.middler-wrapper
+					.middler
+						.news-list
+							article.news-item(v-for="news in data")
+								header
+									.thumbnail
+										a(href="javascript:;")
+											img(v-bind:src="news.thumbnail")
+										a.overlay(href="javascript:;")
+											.title
+												time {{news.updated_time}}
+												h4 {{news.title}}
+								.content {{news.excerpt}}
+								footer.footer.right
+									a(v-bind:href="'/news/' + news.id")
+										span read more &#9656;
 
 </template>
 
 <script>
 import Navigation from './Navigation.vue'
+import Api from '../api'
 // Expose Jquery Globally.
 import $ from 'jquery'
 window.jQuery = window.$ = $
@@ -21,10 +39,36 @@ export default {
   },
   data () {
     return {
-      msg: 'Hello Vue'
+      loading: false,
+      data: null,
+      error: null
     }
   },
-  mounted () {}
+  mounted () {
+    this.fetchData()
+  },
+  updated () {
+    $('#scene').parallax()
+    $('.sticker').sticky({
+      topSpacing: 0,
+      zIndex: 999
+    })
+  },
+  methods: {
+    fetchData () {
+      this.error = this.data = null
+      this.loading = true
+      Api.getNewsHome((err, data) => {
+        this.loading = false
+        if (err) {
+          this.error = err.toString()
+        } else {
+          this.data = data
+          console.log(this.data)
+        }
+      })
+    }
+  }
 }
 </script>
 
